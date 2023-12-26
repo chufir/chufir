987b6c10ac9db969dfce7f29688a4a8263f27ec8
Ahmed Samir <ahmedsamir2699@gmail.com>
2023-12-26 14:53:51 +0200

    update9

--- modules/Car/Views/admin/settings/form-search.blade.php
+++ modules/Car/Views/admin/settings/form-search.blade.php
@@ -21,9 +21,7 @@
                             $types = [
                                 'service_name'=>__("Service name"),
                                 'location'=>__("Location"),
-                                'destination'=>__("Destination"),
                                 'date'=>__("Date"),
-                                'car_type'=>__("Car Type"),
                                 'attr'=>__("Attribute"),
                             ];
                             $attrs = \Modules\Core\Models\Attributes::where('service', 'car')->get();
