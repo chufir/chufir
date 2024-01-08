2a951ba4eaa946808cbd279e4271f8e3d3169d0e
Ahmed Samir <ahmedsamir2699@gmail.com>
2024-01-08 13:31:20 +0200

    update

--- themes/GoTrip/Layout/common/search/fields/date.blade.php
+++ themes/GoTrip/Layout/common/search/fields/date.blade.php
@@ -2,18 +2,18 @@
     <div class="date-wrapper" data-x-dd-click="searchMenu-date">
         <h4 class="text-15 fw-500 ls-2 lh-16">{{ $field['title'] }}</h4>
 
-        <div class="text-14 text-light-1 ls-2 lh-16 check-in-out-render">
+        {{-- <div class="text-14 text-light-1 ls-2 lh-16 check-in-out-render">
             <span class="js-first-date render check-in-render">{{Request::query('start',display_date(strtotime("today")))}}</span>
             -
             <span class="js-last-date render check-out-render">{{Request::query('end',display_date(strtotime("+1 day")))}}</span>
-        </div>
-        {{-- <div class="text-14 text-light-1 ls-2 lh-16 check-in-out-render">
+        </div> --}}
+        <div class="text-14 text-light-1 ls-2 lh-16 check-in-out-render">
             <span class="js-first-date render check-in-render">{{Request::query('start', date('Y-m-d', strtotime("today")))}}
             </span>
             -
             <span class="js-last-date render check-out-render">{{Request::query('end', date('Y-m-d', strtotime("+1 day")))}}
             </span>
-        </div> --}}
+        </div>
     </div>
     <input type="hidden" class="check-in-input" value="{{Request::query('start',display_date(strtotime("today")))}}" name="start">
     <input type="hidden" class="check-out-input" value="{{Request::query('end',display_date(strtotime("+1 day")))}}" name="end">
